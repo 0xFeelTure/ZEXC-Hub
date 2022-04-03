e4e4b2332dcbf759509ebcbd2ff073d9c15ccd46
WANZE0 <92678759+WANZE0@users.noreply.github.com>
2022-04-03 15:10:39 +0700

    Update ZEXCMAIN.lua

--- ZEXCMAIN.lua
+++ ZEXCMAIN.lua
@@ -1,1 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/WANZE0/ZEX-F2U-/main/ZEXCSUPPORT.lua'))()
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/WANZE0/ZEX-F2U-/main/ZEXCMAIN.lua'))()
